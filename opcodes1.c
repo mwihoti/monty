2bdd5ec1fc22a5ced6fae5559d4e4252afd379d4
mwihoti <danielmwihoti@gmail.com>
2024-01-17 21:27:34 -0800

    queue

--- opcodes1.c
+++ opcodes1.c
@@ -19,7 +19,7 @@ void op_push(stack_t **head, unsigned int curline)
 	{
 		if(!isdigit(st.arg[j]) && st.arg[j] != '-')
 		{
-			dprintf(2, "L%u: "", curline);
+			dprintf(2, "L%u: ", curline);
 			dprintf(2, "usage: push integer\n");
 			free_st();
 			exit(EXIT_FAILURE);
@@ -31,4 +31,88 @@ void op_push(stack_t **head, unsigned int curline)
 	else
 		add_nodeend(head, n);
 }
+/**
+ * op_pall - prints stack values
+ * @head: head of linked list
+ * @curline: number of lines
+ * Return: nothing
+ */
+void op_pall(stack_t **head, unsigned int curline)
+{
+	stack_t *nm;
+	(void)curline;
 
+	nm = *head;
+
+	while (nm)
+	{
+		printf("%d\n", nm->n);
+		nm = nm->next;
+	}
+}
+/**
+ * op_pint - prints top value of stack
+ *
+ * @head: link list head
+ * curline: line number
+ * Return: nothing
+ */
+void op_pint(stack_t **head, unsigned int curline)
+{
+	(void)curline;
+
+	if(*head == NULL)
+	{
+		dprintf(2, "L%u: ", curline);
+		dprintf(2, "can't pint, stack empty\n");
+		free_st();
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
+/**
+ * op_pop - remove stack elements from top
+ * @head: linked list head
+ * @curline: line number
+ * Return: nothing
+ */
+void op_pop(stack_t **head, unsigned int curline)
+{
+	stack_t *nm;
+	if (head == NULL || *head == NULL)
+	{
+		dprintf(2, "L%u: can't pop an empty stack\n", curline);
+		free_st();
+		exit(EXIT_FAILURE);
+	}
+	nm = *head;
+	*head = (*head)->next;
+	free(nm);
+}
+/**
+ * pop_swap - swaps top 2 element in stack
+ * @head: head of linked list
+ * curline: line number
+ * Return: nothing
+ */
+void op_swap(stack_t **head, unsigned int curline)
+{
+	int k = 0;
+	stack_t *nm = NULL;
+	nm = *head;
+
+	for (; nm != NULL; nm = nm->next, k++);
+
+	if (k < 2)
+	{
+		dprintf(2, "L%u: can't swap, stack too short\n", curline);
+		free_st();
+		exit(EXIT_FAILURE);
+	}
+	nm = *head;
+	*head = (*head)->next;
+	nm->next = (*head)->next;
+	nm->prev = *head;
+	(*head)->next = nm;
+	(*head)->prev = NULL;
+}
