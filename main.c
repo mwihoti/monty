669e4acca8e1950b23585eb9b1a1aab5e7bca3ae
Daniel Edward Mwihoti <86232216+mwihoti@users.noreply.github.com>
2024-01-19 03:41:15 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,48 @@
 #include "monty.h"
 
-mont_t st;
+/**
+ * main - enry point
+ *
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: 0 o success
+ */
+int main(int argc, char *argv[])
+{
+	mont_t st;
+
+	void (*f)(stack_t **stack, unsigned int line_number);
+	FILE *fd;
+	size_t size = 256;
+	ssize_t numlines = 0;
+	char *lines[2] = {NULL, NULL};
+
+	fd = checks_input(argc, argv);
+	start_st(fd);
+	numlines = getline(&st.buff, &size, fd);
+	while (numlines != -1)
+	{
+		lines[0] = _strtoky(st.buff, " \t\n");
+		if (lines[0] && lines[0][0] != '#')
+		{
+			f = get_ops(lines[0]);
+			if (!f)
+			{
+				fprintf(stderr, "L%u: ", st.curr);
+				fprintf(stderr, "unknown instructions %s\n", lines[0]);
+				free_st();
+				exit(EXIT_FAILURE);
+			}
+			st.arg = _strtoky(NULL, "\t\n");
+			f(&st.head, st.curr);
+		}
+		numlines = getline(&st.buff, &size, fd);
+		st.curr++;
+	}
+	free_st();
+
+	return (0);
+}
 
 /**
  * free_st - frees global variable
@@ -53,46 +95,4 @@ FILE *checks_input(int argc, char *argv[])
 	}
 	return (fd);
 }
-/**
- * main - enry point
- *
- * @argc: arguement count
- * @argv: arguement vector
- * Return: 0 o success
- */
-int main(int argc, char *argv[])
-{
-	mont_t st;
 
-	void (*f)(stack_t **stack, unsigned int line_number);
-	FILE *fd;
-	size_t size = 256;
-	ssize_t numlines = 0;
-	char *lines[2] = {NULL, NULL};
-
-	fd = checks_input(argc, argv);
-	start_st(fd);
-	numlines = getline(&st.buff, &size, fd);
-	while (numlines != -1)
-	{
-		lines[0] = _strtoky(st.buff, " \t\n");
-		if (lines[0] && lines[0][0] != '#')
-		{
-			f = get_ops(lines[0]);
-			if (!f)
-			{
-				fprintf(stderr, "L%u: ", st.curr);
-				fprintf(stderr, "unknown instructions %s\n", lines[0]);
-				free_st();
-				exit(EXIT_FAILURE);
-			}
-			st.arg = _strtoky(NULL, "\t\n");
-			f(&st.head, st.curr);
-		}
-		numlines = getline(&st.buff, &size, fd);
-		st.curr++;
-	}
-	free_st();
-
-	return (0);
-}
