4ef7bb3d735c921f6e2a15ab729af7ffea3352c9
mwihoti <danielmwihoti@gmail.com>
2024-01-20 01:40:32 -0800

    nop

--- getop.c
+++ getop.c
@@ -15,6 +15,7 @@ void (*get_ops(char *opc))(stack_t **stack, unsigned int line_number)
 		{"pop", op_pop},
 		{"swap", op_swap},
 		{"add", op_add},
+		{"nop", op_nop},
 		{NULL, NULL}
 	};
 	int i;
