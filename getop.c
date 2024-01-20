cbb883d87b55d6c0fc055c8608b37ab44040394e
mwihoti <danielmwihoti@gmail.com>
2024-01-20 00:02:53 -0800

    montyudate

---
+++ getop.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * get_ops - select the correct opcode to perform
+ * @opc: opcode passed
+ *
+ * Return: pointer to the function that executes the opcode
+ */
+void (*get_ops(char *opc))(stack_t **stack, unsigned int line_number)
+{
+	instruction_t instruct[] = {
+		{"push", op_push},
+		{"pall", op_pall},
+		{"pint", op_pint},
+		{"pop", op_pop},
+		{"swap", op_swap},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; instruct[i].opcode; i++)
+	{
+	if (get_strcmp(instruct[i].opcode, opc) == 0)
+		break;
+	}
+	return (instruct[i].f);
+}
